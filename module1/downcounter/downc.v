4305338be3a8139e2f7147d40383c22f4b8694e5
Rishit <puligaddarishit@gmail.com>
2026-08-30 05:46:19 +0200

    DownCounter code which is functional and readable

--- module1/downcounter/downc.v
+++ module1/downcounter/downc.v
@@ -1,12 +1,15 @@
 module downc(
   output reg [3:0] y,
-  input clk, rst_n
+  input            clk, 
+  input            rst_n
 );
 
   always @(posedge clk or negedge rst_n) begin
-    if (!rst_n)
+    if (!rst_n) begin
       y <= 4'b1111;
-    else 
+    end
+    else begin 
       y <= y - 1;
+    end
   end
 endmodule
