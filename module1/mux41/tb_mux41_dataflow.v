e2c4ffe8c5084feba4f4cea78b701f22117a9b83
Rishit <puligaddarishit@gmail.com>
2026-08-30 05:58:48 +0200

    4:1 mux dataflow code

--- module1/mux41/tb_mux41_dataflow.v
+++ module1/mux41/tb_mux41_dataflow.v
@@ -1,6 +1,6 @@
 module tb_mux41_dataflow;
-  reg s1, s0;
-  reg i0, i1, i2, i3;
+  reg  s1, s0;
+  reg  i0, i1, i2, i3;
   wire y;
 
   mux41_dataflow dut (.y(y),
@@ -9,17 +9,15 @@ module tb_mux41_dataflow;
                       );
 
   initial begin
-    $display("time = %t, y = %0b| s0 = %0b, s1 = %0b| i0 = %0b, i1 = %0b, i2 = %0b, i3 = %0b", $time, y, s0, s1, i0, i1, i2, i3);
-    $monitor("time = %t, y = %0b| s0 = %0b, s1 = %0b| i0 = %0b, i1 = %0b, i2 = %0b, i3 = %0b", $time, y, s0, s1, i0, i1, i2, i3);
-
-    i0 = 1'b0; i1 = 1'b1; i2 = 1'b0; i3 = 1'b1;
-    s0 = 1'b0; s1 = 1'b0;
+        i0 = 1'b0; i1 = 1'b1; i2 = 1'b0; i3 = 1'b1;
+        s0 = 1'b0; s1 = 1'b0;
     #10 s0 = 1'b1; s1 = 1'b0;
     #10 s0 = 1'b0; s1 = 1'b1;
     #10 s0 = 1'b1; s1 = 1'b1;
   end
 
   initial begin
+    $monitor("time = %t, y = %0b| s0 = %0b, s1 = %0b| i0 = %0b, i1 = %0b, i2 = %0b, i3 = %0b", $time, y, s0, s1, i0, i1, i2, i3);
     $dumpfile("mux41_dataflow.vcd");
     $dumpvars(0, tb_mux41_dataflow);
   end
