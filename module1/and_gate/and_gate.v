91427998260ac569b3b4c5ceabb59a1bc3370539
Rishit <puligaddarishit@gmail.com>
2026-08-30 05:33:51 +0200

    Made the And gate (dataflow) code more readable

--- module1/and_gate/and_gate.v
+++ module1/and_gate/and_gate.v
@@ -1,8 +1,8 @@
 `timescale 1ps/1ps
 module and_gate (
   output y,
-  input a,
-  input b
+  input  a,
+  input  b
 );
   assign y = a & b;
 endmodule
--- module1/and_gate/tb_and.v
+++ module1/and_gate/tb_and.v
@@ -1,6 +1,6 @@
 `timescale 1ps/1ps
 module tb_and;
-  reg a, b;
+  reg  a, b;
   wire y;
 
   and_gate dut(y, a, b);
@@ -10,7 +10,7 @@ module tb_and;
     $monitor("time=%t A = %0b| B = %0b| y = %0b", $time, a, b, y);
     $dumpfile("and_wave.vcd");
     $dumpvars(0, tb_and);
-    a = 0; b = 0;
+        a = 0; b = 0;
     #10 a = 1; b = 0;
     #10 a = 0; b = 1;
     #10 a = 1; b = 1;
